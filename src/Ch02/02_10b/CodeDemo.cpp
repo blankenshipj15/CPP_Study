b282224f57ec4716175b750e8ec1805d3098cef2
Jesse Blankenship <blankenshipj15@gmail.com>
2025-08-16 22:06:01 +0000

    updates for ch 2 sections 9 and 10

--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -7,6 +7,18 @@
 
 int main(){
     
+    double targetX;
+    int32_t spriteX;
+    uint32_t playerX;
+
+    targetX = -123.45;
+    spriteX = targetX;
+    playerX = targetX;
+
+    std::cout << "Target X (double): " << targetX << std::endl;
+    std::cout << "Sprite X (integer): " << spriteX << std::endl; 
+    std::cout << "Target X (unsigned integer): " << static_cast<int32_t>(playerX) << std::endl;
+
     std::cout << std::endl << std::endl;
     return 0;
 }
