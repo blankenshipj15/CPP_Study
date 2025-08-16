e36b7bc55626405eee6b84fef1f7f571033d3b47
Jesse Blankenship <blankenshipj15@gmail.com>
2025-08-16 20:56:57 +0000

    usage for user input

--- src/Ch01/01_09b/CodeDemo.cpp
+++ src/Ch01/01_09b/CodeDemo.cpp
@@ -3,9 +3,14 @@
 // Terminal Interaction, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
 
 int main(){
-    std::cout << "Hi There!" << std::endl;
+    
+    std::string name;
+    std::cout << "Hello, what is your name? " << std::flush;
+    std::cin >> name;
+    std::cout << "Hi " << name << ". Welcome to C++!" << std::endl;
 
     std::cout << std::endl << std::endl;
     return 0;
