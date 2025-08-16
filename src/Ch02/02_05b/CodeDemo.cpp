303b802c25f7f03cba2eeef9f22d77fadbfdbcac
Jesse Blankenship <blankenshipj15@gmail.com>
2025-08-16 21:42:51 +0000

    updates for ch 2 topic 5 and 8

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,8 +4,19 @@
 
 #include <iostream>
 
+int a, b = 5; // these are global variables, not preferred. try to keep variables local
+
 int main(){
     
+    bool myFlag; // this is a local variable
+    a = 7;
+    myFlag = false;
+
+    std::cout << "a = " << a << ", b = " << b << ", and flag = " << myFlag << "." <<   std::endl;
+
+    myFlag = true;
+    std::cout << "a + b = " << a+b << " and flag = " << myFlag << "." <<   std::endl;
+
     std::cout << std::endl << std::endl;
     return 0;
 }
